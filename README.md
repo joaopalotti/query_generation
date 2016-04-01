01ecc425ba443f2c555b2d57b426813d9536a395
joaopalotti <joaopalotti@gmail.com>
2016-04-01 14:21:32 +0200

    Updated instructions on how to use this fork and added a script to create queries

--- README.md
+++ README.md
@@ -3,12 +3,20 @@ Annotator toolkit for creating manual queries from information retrieval experim
 
 Uses AnjularJS, Django
 
+## Pre-requiments;
+You should have at least the following packages installed on your machine: django==1.8 and djangorestframework
+You can install them using pip:
+
+> pip install djangorestframework django==1.8
+
+
 ## Setup 
 
 All the data was provided to in CSV format. The process to setup is as follows:
 
 - Run `./manage.py syncdb` to create the db. Follow the instructions.
-- Run the `python import_patients.py db.sqlite3 all-trec-cds-topics.json` to import the set of queries. Or choose alternative JSON import file.
+- Run `python parse_txt_to_create_topics.py selection-50-queries/` to create a file called topics\_clef.json
+- Run the `python import_patients.py db.sqlite3 topics_clef.json` to import the set of queries.
 - Start the webserver with `./manage.py runserver`
 - Go to `http://localhost:8000/<n>`, where `n` is the nth query you want to start annotating.
 
---
+++ parse_txt_to_create_topics.py
@@ -0,0 +1,43 @@
+"""
+Intructions: A folder with some txt topic is giving as input to this program.
+It then parses one by one of the txt posts and retrieves the initial user post,
+generating a json file that is accepted by the query_generator django db
+"""
+
+import sys
+import json
+import glob
+
+if len(sys.argv) < 1:
+    print "python parse_txt_to_create_topics.py folder_with_a_bunch_of_html_files"
+    sys.exit(0)
+
+inputf = sys.argv[1]
+files = glob.glob(inputf + "/*.txt")
+
+def get_post(fname):
+    f = open(fname, "r")
+    lines = f.readlines()
+    return lines[0] + "\n" + lines[3]
+
+rows = []
+for j, fname in enumerate(files):
+
+    row = {}
+    post = get_post(fname)
+
+    print "Processing topic %d: %s" % (j+1,fname)
+
+    # fields used by query_generator
+    row['description'] = post
+    row['queryType'] = "diagnosis"
+    row['qId'] = fname
+    rows.append(row)
+
+outfile = "topics_clef.json"
+outf = open(outfile, "w")
+
+json.dump(rows, outf,  sort_keys=True, indent=4, separators=(",",":"))
+outf.close()
+print "DONE: %s created." % (outfile)
+
